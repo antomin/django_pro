4cd4d14744928dfe190a2966db1b31c4631e2e76
Anton Mindlin <anton.mindlin@gmail.com>
2023-05-02 11:11:26 +0200

    ch4

---
+++ pages/tests.py
@@ -0,0 +1,26 @@
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
+
+from .views import HomePageView
+
+
+class HomepageTest(SimpleTestCase):
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'HomePage')
+
+    def test_homepage_contains_not_correct_html(self):
+        self.assertNotContains(self.response, 'Hey boy!')
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
