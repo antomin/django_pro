e748e6ca8047d99a233e25359750e2ccf06c46bc
Anton Mindlin <anton.mindlin@gmail.com>
2023-05-16 12:34:17 +0200

    ch14

--- books/urls.py
+++ books/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, SearchResultsListView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_result'),
 ]
